8d70bb582dd7cd96c55e87d75d7ccbc4830785a8
yonje <yonjeremy@gmail.com>
2019-03-05 21:08:35 +0000

    coded sha256 algorithm (without preprocessing)

--- sha.c
+++ sha.c
@@ -10,7 +10,18 @@
 
 void sha256();
 
+// see sections 4.1.2 for definitions
+uint32_t sig0(uint32_t x);
+uint32_t sig1(uint32_t x);
+
+// see section 3.2 for definitions
+uint32_t rotr(uint32_t n, uint32_t x);
+uint32_t shr(uint32_t n, uint32_t x);
+
+
 int main(int argc, char *argv[]){
+
+   sha256();
 
    return 0;
 }
@@ -36,4 +47,66 @@ void sha256(){
       0x1f83d9ab,
       0x5be0cd19
    };
+
+   // the current message block
+   uint32_t M[16];
+
+   // for looping
+   int t;
+
+   // From page 22, W[t] = M[t] for 0 <= t <= 15
+   for(t=0; t<16; t++){
+      W[t] = M[t];
+   }
+
+   // From page 22, W[t] = ...
+   for(t=16; t<64; t++){
+      sig1(W[t-2]) + W[t-7] + sig0(W[t-15]) + W[t-16];
+   }
+
+   // initialise a,b,c,d,e,f,g,h as per step 2, page 22
+   a = H[0]; b = H[1]; c = H[2]; d = H[3]; 
+   e = H[4]; f = H[5]; g = H[6]; h = H[7];
+
+   // step 3
+   for (t = 0; t < 64; t++){
+      T1 = h + SIG_1(e) + Ch(e,f,g) + K[t] + W[t];
+      T2 = SIG_0(a) + Maj(a,b,c);
+      h = g;
+      g = f;
+      f = e;
+      e = d + T1;
+      d = c;
+      c = b;
+      b = a;
+      a = T1 + T2;
+   }
+
+   // step 4
+   H[0] = a + H[0];
+   H[1] = b + H[1];
+   H[2] = c + H[2];
+   H[3] = d + H[3];
+   H[4] = e + H[4];
+   H[5] = f + H[5];
+   H[6] = g + H[6];
+   H[7] = h + H[7];
+
+}
+
+uint32_t rotr(uint32_t n, uint32_t x){
+   return (x >> n) | (x << (32-n)) ;
+}
+
+uint32_t shr(uint32_t n, uint32_t x){
+   return (x >> n);
+}
+
+uint32_t sig0(uint32_t x){
+   // see section 3.2 and 4.1.2 for definitions
+   return (rotr(7,x) ^ rotr(18,x) ^ shr(3,x));
+}
+uint32_t sig1(uint32_t x){
+   // see section 3.2 and 4.1.2 for definitions
+   return (rotr(17,x) ^ rotr(19,x) ^ shr(10,x));
 }
