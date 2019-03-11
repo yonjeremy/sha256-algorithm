2f6446d4a66735dafe1778d49b8557fc2d754d6b
yonje <yonjeremy@gmail.com>
2019-03-11 20:40:04 +0000

    added k values

--- sha.c
+++ sha.c
@@ -36,6 +36,26 @@ int main(int argc, char *argv[]){
 
 void sha256(){
 
+   // the k constants, defined in section 4.2.2
+   uint32_t K[] = {
+      0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 
+      0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+      0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 
+      0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+      0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 
+      0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+      0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 
+      0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+      0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 
+      0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+      0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 
+      0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+      0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 
+      0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+      0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 
+      0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+   };
+
    // message schedule (section 6.2)
    uint32_t W[64];
    // working variables (section 6.2)
@@ -57,10 +77,10 @@ void sha256(){
    };
 
    // the current message block
-   uint32_t M[16];
+   uint32_t M[16] = {0,0,0,0,0,0,0,0};
 
    // for looping
-   int t;
+   int i,t;
 
    // From page 22, W[t] = M[t] for 0 <= t <= 15
    for(t=0; t<16; t++){
@@ -78,8 +98,8 @@ void sha256(){
 
    // step 3
    for (t = 0; t < 64; t++){
-      T1 = h + SIG_1(e) + Ch(e,f,g) + K[t] + W[t];
-      T2 = SIG_0(a) + Maj(a,b,c);
+      T1 = h + SIG1(e) + Ch(e,f,g) + K[t] + W[t];
+      T2 = SIG0(a) + Maj(a,b,c);
       h = g;
       g = f;
       f = e;
@@ -100,6 +120,8 @@ void sha256(){
    H[6] = g + H[6];
    H[7] = h + H[7];
 
+
+   printf("%x %x %x %x %x %x %x %x ", H[0],H[1],H[2],H[3],H[4],H[5],H[6],H[7]);
 }
 
 uint32_t rotr(uint32_t n, uint32_t x){
@@ -121,16 +143,17 @@ uint32_t sig1(uint32_t x){
 
 // see section4.1.2 for definitions
 uint32_t SIG0(uint32_t x){
-
+   return (rotr(2,x) ^ rotr(13,x) ^ rotr(22,x));
 }
 uint32_t SIG1(uint32_t x){
+   return (rotr(6,x) ^ rotr(11,x) ^ rotr(25,x));
 
 }
 
 // see section4.1.2 for definitions
 uint32_t Ch(uint32_t x, uint32_t y, uint32_t z){
-   return (x & y) ^ ((!x) & z);
+   return ((x & y) ^ ((!x) & z));
 }
 uint32_t Maj(uint32_t x, uint32_t y, uint32_t z){
-
+   return ((x & y) ^ (x & z) ^ (y & z));
 }
