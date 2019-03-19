55d7c0c0bbc2f871ce84fd567a07bf9df1e4e378
yonje <yonjeremy@gmail.com>
2019-03-19 10:29:08 +0000

    padding for sha256

--- sha.c
+++ sha.c
@@ -174,6 +174,7 @@ int main(int args, char *argv[]){
    union msgblock M;
 
    uint64_t nobytes;
+   uint64_t nobits = 0;
 
    FILE* f;
 
@@ -181,8 +182,24 @@ int main(int args, char *argv[]){
    
    while (!feof(f)){
       nobytes = fread(M.e,1,64,f);
-      printf("%llu\n", nobytes);
+      nobits = nobits + (nobytes * 8);
+      if (nobytes < 56){
+         printf("I've found a block with less than 55 bytes\n");
+         M.e[nobytes] = 0x80;
+         while (nobytes < 56){
+            nobytes = nobytes + 1;
+            M.e[nobytes] = 0x00;
+         }
+         M.s[7] = nobits;
+      }
+
    }
    fclose(f);
+
+   for (int i = 0; i<64; i++)
+      printf("%x ", M.e[i]);
+   printf("\n");   
+   
+
    return 0;
 }
